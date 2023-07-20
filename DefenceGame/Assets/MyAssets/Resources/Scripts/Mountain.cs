c0fff01fc72ac74d3407f228207c1916a756adf8
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-07-20 17:57:48 +0900

    Add. Prefabs

---
+++ DefenceGame/Assets/MyAssets/Resources/Scripts/Mountain.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO 산 타일들 위부터 order in layer 설정
+public class Mountain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
